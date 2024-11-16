95d5da813b0afb0c5ea11925125a9f7b3c02cb4f
xhdeng <xinhaodeng.thu@gmail.com>
2024-11-16 09:28:12 +0800

    ARES extension

--- scripts/ARES.sh
+++ scripts/ARES.sh
@@ -1,15 +1,26 @@
 dataset=CW
 
+for filename in train valid test
+  do 
+      python -u exp/dataset_process/gen_mtaf.py \
+        --dataset ${dataset} \
+        --seq_len 10000 \
+        --in_file ${filename}
+  done
+
 python -u exp/train.py \
   --dataset ${dataset} \
   --model ARES \
   --device cuda:7 \
-  --feature DIR \
-  --seq_len 10000 \
+  --train_file mtaf_train \
+  --valid_file mtaf_valid \
+  --feature MTAF \
+  --seq_len 8000 \
   --train_epochs 300 \
-  --batch_size 64 \
-  --learning_rate 0.0014 \
+  --batch_size 512 \
+  --learning_rate 2e-3 \
   --optimizer AdamW \
+  --lradj StepLR \
   --eval_metrics Accuracy Precision Recall F1-score \
   --save_metric F1-score \
   --save_name max_f1
@@ -18,8 +29,10 @@ python -u exp/test.py \
   --dataset ${dataset} \
   --model ARES \
   --device cuda:7 \
-  --feature DIR \
-  --seq_len 10000 \
-  --batch_size 256 \
+  --valid_file mtaf_valid \
+  --test_file mtaf_test  \
+  --feature MTAF \
+  --seq_len 8000 \
+  --batch_size 512 \
   --eval_metrics Accuracy Precision Recall F1-score \
   --load_name max_f1
